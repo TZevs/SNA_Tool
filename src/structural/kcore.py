367d6ed2e89f46ca332984a47c0bd7be7481e476
Thalia <148969711+TZevs@users.noreply.github.com>
2026-03-17 19:43:31 +0000

    Finished kcore, returns dictionary of kcore values. Removed kshell as its unnecessary

--- src/structural/kcore.py
+++ src/structural/kcore.py
@@ -1,17 +1,12 @@
 import networkx as nx
 
-def undir_kcore_shell(G):
-    H = G.copy()
-    # Compute k-core decomposition
-    #   - recursively removes the nodes with less than k-max degrees
-    #   - computes the structure of the entire network, or the structure of nodes with at least (k+1) degrees
-    #   - Returns the maximal k-core subgraph (the largets possible value of k)
-    main_core = nx.k_core(H)
+def compute_kcore(G):
+    """
+    - Compute the core number for each node in the graph G
+    - core_number = the largest value k of a k-core containing the node
+    - Using core_number instead of k_core as that returns a subgraph, which is not needed for the analysis, other than for visualisation
+    """
+    core_nums = nx.core_number(G)
 
-    core_nums = nx.core_number(H)
-
-    # Compute k-core decomposition
-    #   - the recursively removed nodes are stored in a bucket == kshell
-    #   - computes the k
-    #   - Returns k-shell nodes
-    main_shell = nx.k_shell(H)
+    # Returns dictionary {node: core_value}
+    return core_nums
