c5c6f5556b03025a40f23e607b0137bc74153f53
Thalia <148969711+TZevs@users.noreply.github.com>
2026-03-23 15:19:15 +0000

    shortend dict names

--- src/community/metrics.py
+++ src/community/metrics.py
@@ -2,29 +2,20 @@ import networkx as nx
 from collections import defaultdict
 
 def compute_intra_inter_degree(G, community_map):
-    intra_deg = defaultdict(lambda: defaultdict(int))
-    inter_deg = defaultdict(lambda: defaultdict(int))
+    intra = defaultdict(lambda: defaultdict(int))
+    inter = defaultdict(lambda: defaultdict(int))
 
     for u, v in G.edges():
         cu, cv = community_map[u], community_map[v]
 
         if cu == cv:
-            intra_deg[cu][u] += 1
-            intra_deg[cv][v] += 1
+            intra[cu][u] += 1
+            intra[cv][v] += 1
         else:
-            inter_deg[cu][u] += 1
-            inter_deg[cv][v] += 1
+            inter[cu][u] += 1
+            inter[cv][v] += 1
 
-    sorted_intra = {
-        comm_id: dict(sorted(nodes.items(), key=lambda item: item[1], reverse=True))
-        for comm_id, nodes in intra_deg.items()
-    }
-    sorted_inter = {
-        comm_id: dict(sorted(nodes.items(), key=lambda item: item[1], reverse=True))
-        for comm_id, nodes in inter_deg.items()
-    }
-
-    return sorted_intra, sorted_inter
+    return intra, inter
 
 def compute_density(G, coms_dict):
     community_density = {}
