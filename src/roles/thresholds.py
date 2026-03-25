ca68c5a0492815de26e9b27f4bc418770068a223
Thalia <148969711+TZevs@users.noreply.github.com>
2026-03-25 17:14:26 +0000

    Implemented functions to compute both local and global thresholds

--- src/roles/thresholds.py
+++ src/roles/thresholds.py
@@ -1,11 +1,18 @@
+def compute_local_thresholds(df):
+    percentiles = [0.25, 0.50, 0.75, 0.90]
+
+    local_df = df[['intra', 'inter', 'local_closeness', 'local_core_num', 'betweenness']]
+
+    local_thresholds = local_df.groupby('community').quantile(percentiles, interpolation='nearest').unstack()
+
+    return local_thresholds
 
 
-def compute_local_thresholds():
+def compute_global_thresholds(df):
+    percentiles = [0.25, 0.50, 0.75, 0.90]
 
+    global_df = df[['degree', 'global_closeness', 'global_core_num', 'betweenness', 'eigenvector', 'trussness']]
 
-    return
+    global_thresholds = global_df.quantile(percentiles, interpolation='nearest')
 
-
-def compute_global_thresholds():
-
-    return
+    return global_thresholds
