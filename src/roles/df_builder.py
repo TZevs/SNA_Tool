f0b231657e5b3695db4e1eb26954c9af374c2feb
Thalia <148969711+TZevs@users.noreply.github.com>
2026-03-27 16:34:21 +0000

    name changes to prevent confusions

--- src/roles/df_builder.py
+++ src/roles/df_builder.py
@@ -1,24 +1,24 @@
 import pandas as pd
 
-def build_metrics_df(G, community_map, intra, inter, local_closeness, core, betweenness, global_closeness, degree, eigen, kcore, ktruss):
+def build_metrics_df(G, node_comms, intra, inter, l_close, l_core, bet, close, deg, eigen, core, truss):
     rows = []
 
     for n in G.nodes():
-        cid = community_map[n]
+        cid = node_comms[n]
 
         rows.append({
             'node': n,
             'community': cid,
-            'degree': degree[n],
+            'degree': deg[n],
             'intra': intra[cid].get(n, 0),
             'inter': inter[cid].get(n, 0),
-            'local_closeness': local_closeness[cid].get(n, 0),
-            'global_closeness': global_closeness[n],
-            'local_core_num': core[cid].get(n, 0),
-            'global_core_num': kcore[n],
-            'betweenness': betweenness[n],
+            'local_closeness': l_close[cid].get(n, 0),
+            'global_closeness': close[n],
+            'local_core_num': l_core[cid].get(n, 0),
+            'global_core_num': core[n],
+            'betweenness': bet[n],
             'eigenvector': eigen[n],
-            'trussness': ktruss[n]
+            'trussness': truss[n]
         })
 
     return pd.DataFrame(rows)
