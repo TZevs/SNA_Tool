0d0b0c9362869b0c2bef259d519244568e493969
Thalia <148969711+TZevs@users.noreply.github.com>
2026-03-31 16:27:12 +0100

    Refactored the df builder, removed intra and inter, added zscore and participation coefficient.

--- src/roles/df_builder.py
+++ src/roles/df_builder.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def build_metrics_df(G, node_comms, intra, inter, l_close, l_core, bet, close, deg, eigen, core, truss):
+def build_metrics_df(G, node_comms, l_close, l_core, bet, close, deg, eigen, core, truss, zscore, part_eff):
     rows = []
 
     for n in G.nodes():
@@ -10,15 +10,15 @@ def build_metrics_df(G, node_comms, intra, inter, l_close, l_core, bet, close, d
             'node': n,
             'community': cid,
             'degree': deg[n],
-            'intra': intra[cid].get(n, 0),
-            'inter': inter[cid].get(n, 0),
             'local_closeness': l_close[cid].get(n, 0),
             'global_closeness': close[n],
             'local_core_num': l_core[cid].get(n, 0),
             'global_core_num': core[n],
             'betweenness': bet[n],
             'eigenvector': eigen[n],
-            'trussness': truss[n]
+            'trussness': truss[n],
+            'local_zscore': zscore[n],
+            'local_P': part_eff[n]
         })
 
     return pd.DataFrame(rows)
