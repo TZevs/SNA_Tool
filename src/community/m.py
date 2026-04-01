a03d94ac51d31b0182e1f932cd81fc373cffb608
Thalia <148969711+TZevs@users.noreply.github.com>
2026-04-01 16:39:08 +0100

    Removed duplicate metric functions. Will be updating to be a stats function

--- src/community/m.py
+++ src/community/m.py
@@ -1,21 +1,4 @@
 import networkx as nx
-from collections import defaultdict
-
-def compute_intra_inter_degree(G, community_map):
-    intra = defaultdict(lambda: defaultdict(int))
-    inter = defaultdict(lambda: defaultdict(int))
-
-    for u, v in G.edges():
-        cu, cv = community_map[u], community_map[v]
-
-        if cu == cv:
-            intra[cu][u] += 1
-            intra[cv][v] += 1
-        else:
-            inter[cu][u] += 1
-            inter[cv][v] += 1
-
-    return intra, inter
 
 def compute_density(G, coms_dict):
     community_density = {}
@@ -33,21 +16,3 @@ def compute_sizes(coms_dict):
         community_sizes[cid] = len(com)
 
     return community_sizes
-
-def compute_community_closeness(G, coms_dict):
-    community_closeness = {}
-
-    for cid, nodes in coms_dict.items():
-        subgraph = G.subgraph(nodes)
-        community_closeness[cid] = nx.closeness_centrality(subgraph)
-
-    return community_closeness
-
-def compute_community_kcore(G, coms_dict):
-    community_node_kcore = {}
-
-    for cid, nodes in coms_dict.items():
-        sub = G.subgraph(nodes)
-        community_node_kcore[cid] = nx.core_number(sub)
-
-    return community_node_kcore
