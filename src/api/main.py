adc1c68f6d99e97d45187eb46db77482bd263bbc
Thalia <148969711+TZevs@users.noreply.github.com>
2026-04-04 16:46:52 +0100

    Created FastAPI app, removed the pipeline call.

--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,13 @@
-from pipelines.pipeline import run_pipeline
+#from pipelines.pipeline import run_pipeline
+from fastapi import FastAPI
+from .routers import node_metrics, recommendations, graph, communities
 
-if __name__ == '__main__':
-    run_pipeline()
+app = FastAPI()
+
+app.include_router(node_metrics.router)
+app.include_router(graph.router)
+app.include_router(recommendations.router)
+app.include_router(communities.router)
+
+# if __name__ == '__main__':
+#     run_pipeline()
