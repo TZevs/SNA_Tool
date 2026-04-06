26a30a35dc03993a1999a1df657a2b3e8978b057
Thalia <148969711+TZevs@users.noreply.github.com>
2026-04-06 20:15:48 +0100

    Implemented 2 routers for endpoints, for local and global data fetching

--- src/api/routers/communities.py
+++ src/api/routers/communities.py
@@ -2,24 +2,68 @@ from fastapi import APIRouter
 import numpy as np
 from src.utils.file_loader import load_csv
 
-router = APIRouter(prefix="/communities")
+router = APIRouter(prefix="/community")
 
 nodes = load_csv("node_data.csv")
 edges = load_csv("edge_data.csv")
+recs = load_csv("local_recs.csv")
+
+@router.get("/ids")
+def get_community_ids():
+    if nodes.empty:
+        return {"status": "missing data", "message": "data upload required"}
+
+    ids = nodes['community'].unique().tolist()
+
+    return {
+        "status": "success",
+        "comm_ids": ids
+    }
 
 @router.get("/{comm_id}")
-def get_community(comm_id: str):
+def get_community(comm_id):
     if nodes.empty or edges.empty:
         return {"status": "missing data", "message": "data upload required"}
 
     df = nodes.loc[nodes['community'] == comm_id]
-    ed_df = edges.loc[edges['source'].isin(df['node'])]
+    valid_nodes = set(df['node'].astype(str))
 
+    ed_df = edges[
+        edges['source'].astype(str).isin(valid_nodes) &
+        edges['target'].astype(str).isin(valid_nodes)
+    ]
     community = df.replace({np.nan: None}).astype(object).to_dict(orient="records")
     comm_edges = ed_df.replace({np.nan: None}).astype(object).to_dict(orient="records")
 
     return {
         "status": "success",
-        "community": community,
+        "nodes": community,
         "edges": comm_edges,
+    }
+
+@router.get('/metrics/{comm_id}')
+def get_metrics(comm_id):
+    if nodes.empty or edges.empty:
+        return {"status": "missing data", "message": "data upload required"}
+
+    df = nodes[['node', 'community', 'local_closeness', 'local_core_num', 'betweenness', 'local_zscore', 'local_P', 'local_role']]
+    metrics = df.loc[nodes['community'] == comm_id]
+    metrics = metrics.replace({np.nan: None}).astype(object).to_dict(orient="records")
+
+    return {
+        "status": "success",
+        "nodes": metrics
+    }
+
+@router.get("/recs/{comm_id}")
+def get_local_recs(comm_id):
+    if recs.empty:
+        return {"status": "missing data", "message": "data upload required"}
+
+    df = recs.loc[nodes['community'] == comm_id]
+    recomms = df.replace({np.nan: None}).astype(object).to_dict(orient="records")
+
+    return {
+        "status": "success",
+        "recs": recomms
     }
---
+++ src/api/routers/global_.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+from src.utils.file_loader import load_csv
+import numpy as np
+
+router = APIRouter(prefix="/global")
+
+node_metrics = load_csv('node_data.csv')
+recs = load_csv('global_recs.csv')
+
+@router.get('/metrics')
+def get_metrics():
+    if node_metrics.empty:
+        return {"status": "missing data", "message": "data upload required"}
+
+    metrics_df = node_metrics[['node', 'degree', 'global_closeness', 'global_core_num', 'betweenness', 'eigenvector', 'trussness', 'global_role']]
+    metrics = metrics_df.replace({np.nan: None}).astype(object).to_dict(orient="records")
+
+    return {
+        "status": "success",
+        "nodes": metrics
+    }
+
+@router.get("/recs")
+def get_global_recs():
+    if recs.empty:
+        return {"status": "missing data", "message": "data upload required"}
+    return {
+        "status": "success",
+        "recs": recs.to_dict(orient="records")
+    }
