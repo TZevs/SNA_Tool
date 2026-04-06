bccfe8c067a6976aba9079f06d130d14ec373b8d
Thalia <148969711+TZevs@users.noreply.github.com>
2026-04-06 20:16:35 +0100

    Making the api requests.

---
+++ frontend/dash_app/client.py
@@ -0,0 +1,40 @@
+import requests
+
+backend_url = "http://localhost:8000"
+
+def fetch_global_metrics():
+    try:
+        return requests.get(f'{backend_url}/global/metrics').json()
+    except Exception as ex:
+        return {"status": "error", "message": str(ex)}
+
+def fetch_global_recs():
+    try:
+        return requests.get(f'{backend_url}/global/recs').json()
+    except Exception as ex:
+        return {"status": "error", "message": str(ex)}
+
+
+def fetch_community_ids():
+    try:
+        return requests.get(f'{backend_url}/community/ids').json()
+    except Exception as ex:
+        return {"status": "error", "message": str(ex)}
+
+def fetch_community_nodes(comm_id):
+    try:
+        return requests.get(f'{backend_url}/community/{comm_id}').json()
+    except Exception as ex:
+        return {"status": "error", "message": str(ex)}
+
+def fetch_community_metrics(comm_id):
+    try:
+        return requests.get(f'{backend_url}/community/metrics/{comm_id}').json()
+    except Exception as ex:
+        return {"status": "error", "message": str(ex)}
+
+def fetch_community_recs(comm_id):
+    try:
+        return requests.get(f'{backend_url}/community/recs/{comm_id}').json()
+    except Exception as ex:
+        return {"status": "error", "message": str(ex)}
