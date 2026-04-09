3426030e1e27f3901aa9d0fa2d45a86e5657f946
Thalia <148969711+TZevs@users.noreply.github.com>
2026-04-09 16:06:56 +0100

    Renaming and uncommenting modularity then returning it.

--- src/community/detect.py
+++ src/community/detect.py
@@ -5,20 +5,20 @@ def detect_communities(G):
     H = G.copy()
 
     # Returns a single list of node sets, 1 per community
-    coms_list = nx.community.louvain_communities(H)
+    comms_list = nx.community.louvain_communities(H)
 
-    # partition_mod = nx.community.modularity(coms_list)
+    mod = nx.community.modularity(G, comms_list)
 
-    node_coms = {}
-    for index, com in enumerate(coms_list):
+    node_comms = {}
+    for index, com in enumerate(comms_list):
         for n in com:
-            node_coms[n] = f'c{index}'
+            node_comms[n] = f'c{index}'
 
     # Dictionary to store the communities and their metrics
-    coms_dict = {}
+    comms = {}
     # Iterate through the list, use enumerate to get the set index's
-    for index, com in enumerate(coms_list):
+    for index, comm in enumerate(comms_list):
         # Assign an ID as a key to each community
-        coms_dict[f'c{index}'] = list(com)
+        comms[f'c{index}'] = list(comm)
 
-    return node_coms, coms_dict
+    return node_comms, comms, mod
