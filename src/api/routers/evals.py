64bc8e1fc1e178e220fc8b829ea94ccc0126296c
Thalia <148969711+TZevs@users.noreply.github.com>
2026-04-12 18:08:37 +0100

    Eval endpoint and router

---
+++ src/api/routers/evals.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+import pandas as pd
+
+router = APIRouter()
+
+@router.get('/evals')
+def get_evals():
+    eval_df = pd.read_csv('../../data/rank_corr_eval.csv')
+
+    return {
+        "status": "success",
+        "evals": eval_df.to_dict(orient='records'),
+    }
