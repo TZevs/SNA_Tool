c3fd1a72e01c876bfc365452beb711f5e9ce8d37
Thalia <148969711+TZevs@users.noreply.github.com>
2026-04-13 12:12:45 +0100

    Correcting file path

--- src/api/routers/evals.py
+++ src/api/routers/evals.py
@@ -5,7 +5,7 @@ router = APIRouter()
 
 @router.get('/evals')
 def get_evals():
-    eval_df = pd.read_csv('../../data/rank_corr_eval.csv')
+    eval_df = pd.read_csv('../../data/processed/rank_corr_eval.csv')
 
     return {
         "status": "success",
