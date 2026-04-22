df1536c8271ebc00f4e842a6c12ac9ce11c9a3e6
Thalia <148969711+TZevs@users.noreply.github.com>
2026-04-22 17:20:41 +0100

    Removed edge api router

--- src/api/main.py
+++ src/api/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from .routers import global_, graph, communities, evals
+from .routers import global_, communities, evals
 
 app = FastAPI()
 
 app.include_router(global_.router)
-app.include_router(graph.router)
 app.include_router(communities.router)
 app.include_router(evals.router)
